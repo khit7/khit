c3ba2b6e8b3d1cefceaa73794332d5e6c7346a04
khit7 <kondapallisk19@gmail.com>
2024-08-06 19:07:59 +1100

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,22 @@
+<?php
+$servername = 'localhost:3306';
+$username = 'root';
+$password = '';
+$database = 'std';
+
+$mysqli = new mysqli($servername, $username, $password, $database);
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+$sql = "SELECT * FROM std_dt ORDER BY PIN";
+
+$stmt = $mysqli->prepare($sql);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$stmt->close();
+$mysqli->close();
+?>
