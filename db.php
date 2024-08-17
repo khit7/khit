ed9f896d1106e8b1f3257bf4e3d251d6e647d25a
khit7 <kondapallisk19@gmail.com>
2024-08-17 20:33:23 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,98 @@
+<?php
+// Configuration
+$servername = 'sql113.infinityfree.com';
+$username = 'if0_36887705';
+$password = 'Ksk6301114021';
+$database = 'if0_36887705_std';
+
+// Create connection
+$mysqli = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+// SQL query
+$sql = "SELECT * FROM std_dt ORDER BY PIN";
+
+// Prepare and execute the query
+$stmt = $mysqli->prepare($sql);
+$stmt->execute();
+
+// Get the results
+$result = $stmt->get_result();
+
+// Close the statement and connection
+$stmt->close();
+$mysqli->close();
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>KHIT</title>
+    <style>
+        table {
+            margin: 0 auto;
+            font-size: large;
+            border: 1px solid black;
+        }
+
+        h1 {
+            text-align: center;
+            color: #006600;
+            font-size: xx-large;
+            font-family: 'Gill Sans', 'Gill Sans MT', 'Calibri', 'Trebuchet MS', 'sans-serif';
+        }
+
+        td {
+            background-color: #E4F5D4;
+            border: 1px solid black;
+        }
+
+        th,
+        td {
+            font-weight: bold;
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+
+        td {
+            font-weight: lighter;
+        }
+    </style>
+</head>
+
+<body>
+    <section>
+        <h1>C20 VSEM Students Data</h1>
+
+        <table>
+            <tr>
+                <th>PIN</th>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Branch</th>
+            </tr>
+
+            <?php
+            while ($rows = $result->fetch_assoc()) {
+            ?>
+                <tr>
+                    <td><?php echo $rows['PIN']; ?></td>
+                    <td><?php echo $rows['Name']; ?></td>
+                    <td><?php echo $rows['Course']; ?></td>
+                    <td><?php echo $rows['Branch']; ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </section>
+</body>
+
+</html>
+
+<?php echo "Hello World!.." ?>
